d9fd0868652c9bf442631abe880973baa627ed5f
Anand Shukla <anands2807@gmail.com>
2026-04-18 10:15:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extends: true}));
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+  res.send("hello world!")
+})
+
+app.get('/test', (req, res)=>{
+  res.status(200).json({"message": "test successful"})
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on ${PORT}`)
+})
---
+++ public/script.js
@@ -0,0 +1,9 @@
+console.log('Netlify client')
+
+// fetch("https://and4web-co-in.onrender.com/test").then(response => {
+//   if(!response.ok){
+//     throw new Error("Fetch Error")
+//   }
+//   return response.json();
+// }).then((data)=>console.log(data));
+
